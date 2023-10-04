728a4c01f4b179b30d6aaba70d8b825f41c6f951
Valerij Ezhov <ezhov.fzzz@gmail.com>
2023-10-04 17:04:45 +0600

    Тут реализована функция send_emai, добавлен логгер

---
+++ mailer.py
@@ -0,0 +1,29 @@
+import smtplib
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+import logging
+import os
+
+def send_email(to, subject, message):
+    try:
+        msg = MIMEMultipart()
+        msg['From'] = os.environ.get('SMTP_EMAIL')
+        msg['To'] = to
+        msg['Subject'] = subject
+
+        msg.attach(MIMEText(message, 'plain'))
+
+        server = smtplib.SMTP(os.environ.get('SMTP_SERVER', 'smtp.mailgun.org'), int(os.environ.get('SMTP_PORT', 587)))
+        server.starttls()
+
+        server.login(msg['From'], os.environ.get('SMTP_PASSWORD'))
+        server.sendmail(os.environ.get('SMTP_EMAIL'), to, msg.as_string())
+        server.quit()
+
+        logging.info(f"Successfully sent email to {to}")
+
+        return True
+
+    except Exception as e:
+        logging.error(f"Failed to send email: {str(e)}")
+        return False
